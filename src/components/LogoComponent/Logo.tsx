e5f9ccff1be5bc3d3db0e7cdab9408f3ce2d96b9
Мария <92936726+Rey-2903@users.noreply.github.com>
2023-11-20 20:18:55 +0500

    Page header layout

---
+++ src/components/LogoComponent/Logo.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../../../public/img/logo.png";
+import logoMin from "../../../public/img/logoMin.png";
+
+import styles from "./styles/Logo.module.scss"
+
+interface LogoProps {
+    width: number;
+    height: number;
+    min?: boolean;
+}
+
+const Logo = ( { width, height, min }: LogoProps ) => {
+    return (
+        <>
+            <div className={styles.logoContainer}>
+                {min ? (
+                    <img width={width} height={height} src={logoMin} alt="Spirit"/>
+                ) : (
+                    <img width={width} height={height} src={logo} alt="Spirit"/>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Logo;
