0c9031310637679fe34a26593ccad030fc56d3e4
Мария <92936726+Rey-2903@users.noreply.github.com>
2023-11-29 04:43:27 +0500

    Component layout Our advantages and How to sign up

---
+++ src/components/InformationIconsComponent/InformationIcons.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Icon from '@ant-design/icons';
+
+import styles from "./styles/InformationIcons.module.scss"
+
+interface InformationIconsProps {
+    icon: any,
+    name: string,
+    description?: string,
+}
+
+const InformationIcons = ( { icon, name, description } : InformationIconsProps ) => {
+    return (
+        <>
+            {description ? (
+                <div className={styles.iconContent}>
+                    <Icon component={icon as React.ForwardRefExoticComponent<any>} className={styles.iconDescr} />
+                    <p className={styles.titleDescr}>{name}</p>
+                    <p className={styles.description}>{description}</p>
+                </div>
+            ) : (
+                <div className={styles.iconContainer}>
+                    <Icon component={icon as React.ForwardRefExoticComponent<any>} className={styles.icon} />
+                    <p className={styles.title}>{name}</p>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default InformationIcons;
