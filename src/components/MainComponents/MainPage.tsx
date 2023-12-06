b1e77638c48bec75b3132d1577b07ac7cf278076
Мария <92936726+Rey-2903@users.noreply.github.com>
2023-12-06 16:23:15 +0500

    Layout of the Active leisure component

--- src/components/MainComponents/MainPage.tsx
+++ src/components/MainComponents/MainPage.tsx
@@ -3,9 +3,10 @@ import PhotoSlider from "../PhotoSlider/PhotoSlider.tsx";
 import SiteStatistic from "../SiteStatistic/SiteStatistic.tsx";
 import OurAdvantages from "../IconComponents/OurAdvantages.tsx";
 import HowSignUp from "../IconComponents/HowSignUp.tsx";
+import FormCustom from "../FormComponent/Form.tsx";
+import ActiveLeisure from "../ActiveLeisureComponents/ActiveLeisure.tsx";
 
 import styles from "./styles/MainPage.module.scss";
-import FormCustom from "../FormComponent/Form.tsx";
 
 function MainPage() {
     return (
@@ -13,6 +14,7 @@ function MainPage() {
             <div className={styles.container}>
                 <Header />
                 <PhotoSlider/>
+                <ActiveLeisure />
                 <OurAdvantages />
                 <SiteStatistic/>
                 <HowSignUp />
