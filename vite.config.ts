99f2ed729cd80fcf5891933aa4da3f8faa52aa06
Roman <romamail.ru365@gmail.com>
2024-06-03 03:19:03 +0500

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-	server: {
-		watch: {
-			usePolling: true
-		},
-		hmr: {
-			overlay: true,
-			clientPort: 5173
-		},
-		host: true,
-		strictPort: true,
-		port: 5173
-	}
+  plugins: [react()]
 })
