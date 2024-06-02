bb689c38210c3c9c8b55e9b9264c5b351a249d8d
Roman <romamail.ru365@gmail.com>
2024-06-03 03:37:07 +0500

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
 	server: {
 		host: true,
 		strictPort: true,
-		port: 8080
+		port: 8080,
+		hmr: {
+			overlay: true,
+			protocol: ws,
+			clientPort: 8080
+		}
 	}
 })
