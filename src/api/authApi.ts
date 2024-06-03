d9fa5037e5e2a7673ebb4602a579d55bf50cbea5
Roman <romamail.ru365@gmail.com>
2024-06-03 15:15:32 +0500

    change base url

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://bba34l2b0gum3icighb8.containers.yandexcloud.net'
 export const authApi = axios.create({
     baseURL: BASE_URL
 })
